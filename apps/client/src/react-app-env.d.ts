2602acbc0ca8ec280d92e6bd6715bd4563b049a6
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-11-26 16:58:23 -0300

    🏷️ Add type for process.env

--- apps/client/src/react-app-env.d.ts
+++ apps/client/src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // node env
+    NODE_ENV: 'development' | 'production' | 'test',
+
+    // feature flags
+    REACT_APP_SHOW_ROUTES:        'true' | 'false' | '',
+    REACT_APP_SHOW_QUEM_SOMOS:    'true' | 'false' | '',
+    REACT_APP_SHOW_O_QUE_FAZEMOS: 'true' | 'false' | '',
+    REACT_APP_SHOW_POEMA:         'true' | 'false' | '',
+
+    // url of APIs used
+    REACT_APP_STRAPI_URL: string
+  }
+}

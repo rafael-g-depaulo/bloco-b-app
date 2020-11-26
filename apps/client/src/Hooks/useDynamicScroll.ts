8e40219a81ac8130e16fb49931f25d0a74987f12
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-11-26 17:01:45 -0300

    ✨ Finish service page

--- apps/client/src/Hooks/useDynamicScroll.ts
+++ apps/client/src/Hooks/useDynamicScroll.ts
@@ -5,18 +5,18 @@ const scrollToTop = () => {
   document.querySelector("#root")?.scrollIntoView({ behavior: "smooth" })
 }
 
-const useDynamicScroll = () => {
+const useDynamicScroll = (delay: number = 750) => {
   
   const history = useHistory()
   const { hash } = history.location
   useEffect(() => {
     const element = hash !== "" && document.querySelector(hash)
     if (element) {
-      setTimeout(() => element.scrollIntoView({ behavior: 'smooth' }), 800)
+      setTimeout(() => element.scrollIntoView({ behavior: 'smooth' }), delay)
     } else {
       scrollToTop()
     }
-  }, [hash])
+  }, [hash, delay])
 
 }
 
--- apps/client/src/Routes/Services/Content/index.tsx
+++ apps/client/src/Routes/Services/Content/index.tsx
@@ -2,10 +2,29 @@ import React, { FC } from "react"
 import styled from "styled-components"
 
 import { useFazemosList } from "Api/OQueFazemos"
+import Loading from "Components/Loading"
+import Markdown from "Components/Markdown"
 
 const List = styled.div`
   display: flex;
   flex-direction: column;
+
+  padding: 40px 0.8em;
+
+  & > article {
+    margin-left: 3em;
+    margin-right: 3em;
+  }
+`
+
+const Hr = styled.hr`
+  width: 95%;
+  height: 1px;
+  background-color: #a91e34;
+  color: #a91e34;
+  border-width: 0;
+
+  margin: 2.4em 0;
 `
 
 export const Content: FC = () => {
@@ -13,12 +32,21 @@ export const Content: FC = () => {
 
   const { data } = servicesList
 
-  if (!data) return <div>a</div>
+  if (!data) return <List><Loading /></List>
+
+  const descricoes = data.map(servico => servico.descricaoLonga)
 
   return (
     <List>
       {/* TODO: Add service content here */}
-      <pre>{JSON.stringify(data[0], null, 2)}</pre>
+      {descricoes.map((descricao, i) => (
+        <>
+        <article key={i}>
+          <Markdown source={descricao}/>
+        </article>
+        { i !== descricoes.length-1 && <Hr />}
+        </>
+      ))}
     </List>
   )
 }

9f78f894a9cf01356f40fa2e8de9dcd8e30aedd2
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-11-26 16:58:03 -0300

    ✨ Add loading spinner

--- apps/client/src/Components/Loading.tsx
+++ apps/client/src/Components/Loading.tsx
@@ -1,8 +1,45 @@
-import React, { FC } from 'react'
+import React from 'react'
+import styled, { css } from 'styled-components'
 
-export const Loading: FC = () => {
+import { PulseLoader } from 'react-spinners'
+
+const Centralize = styled.div<{ fullpage: boolean }>`
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  max-height: 100%;
+  height: 100%;
+
+  padding: 3em;
+
+  ${({ fullpage }) => fullpage && css`height: 100vh;` }
+`
+
+export const Loading = ({
+  fullpage = false,
+  ...props
+}) => {
+
+  const size = 
+    fullpage ? 80 :
+    undefined
+  
+  const margin = 
+    fullpage ? 20 :
+    undefined
+
   return (
-    <div>loading...</div>
+    <Centralize fullpage={fullpage} {...props}>
+      <PulseLoader
+        color="#d12b15"
+        size={size}
+        margin={margin}
+      />
+    </Centralize>
+    // <div>loading...</div>
   )
 }
 

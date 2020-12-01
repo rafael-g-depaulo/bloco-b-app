f832b4ec8dc3143e6d6761db2064756a525f4ae9
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-11-30 22:45:34 -0300

    🚩 Add showPortfolio feature flag

--- apps/client/src/react-app-env.d.ts
+++ apps/client/src/react-app-env.d.ts
@@ -10,6 +10,7 @@ declare namespace NodeJS {
     REACT_APP_SHOW_O_QUE_FAZEMOS: 'true' | 'false' | '',
     REACT_APP_SHOW_POEMA:         'true' | 'false' | '',
     REACT_APP_SHOW_SERVICOS:      'true' | 'false' | '',
+    REACT_APP_SHOW_PORTFOLIO:     'true' | 'false' | '',
 
     // url of APIs used
     REACT_APP_STRAPI_URL: string

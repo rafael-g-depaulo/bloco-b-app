3b267516aae26bf8323184a4ee7fafb3d4571276
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2020-11-30 22:57:37 -0300

    ✨ ♻️ Add link to portfolio in Wireframe

--- apps/client/src/Components/Wireframe.tsx
+++ apps/client/src/Components/Wireframe.tsx
@@ -1,4 +1,4 @@
-import { showOQueFazemos, showQuemSomos, showUltimosAnos } from "FeatureFlags"
+import { showOQueFazemos, showPortfolio, showQuemSomos, showUltimosAnos } from "FeatureFlags"
 import React, { FC, RefObject, useRef } from "react"
 import Footer from "Components/Footer/Footer"
 import Header from "Components/Header/Header"
@@ -16,14 +16,32 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
 `
+type OnClickFn = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 
 export interface WireframeProps {
-  onClickHome?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
-  onClickSomos: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
-  onClickServicos: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
-  onClickUltimosAnos: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
-  onClickContato?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
+  onClickHome?: OnClickFn,
+  onClickSomos: OnClickFn,
+  onClickServicos: OnClickFn,
+  onClickUltimosAnos: OnClickFn,
+  onClickContato?: OnClickFn,
 }
+
+const NavItems: FC<WireframeProps> = ({
+  onClickHome,
+  onClickSomos,
+  onClickServicos,
+  onClickUltimosAnos,
+  onClickContato,
+}) => (
+  <>
+    <NavItem to="/" onClick={onClickHome}>Home</NavItem>
+      { showQuemSomos   && <NavItem to="/#sobre-nos" onClick={onClickSomos}>Quem Somos</NavItem> }
+      { showOQueFazemos && <NavItem to="/services" onClick={onClickServicos}>Serviços</NavItem> }
+      { showUltimosAnos && <NavItem to="/#fizemos-recentemente" onClick={onClickUltimosAnos}>O que fizemos nos últimos anos</NavItem> }
+      { showPortfolio   && <NavItem to="/portifolio">Portifólio</NavItem> }
+    <NavItem to="/#contato" onClick={onClickContato}>Contato</NavItem>
+  </>
+)
 
 export const Wireframe: FC<WireframeProps> = ({
   onClickHome,
@@ -36,7 +54,7 @@ export const Wireframe: FC<WireframeProps> = ({
   
   const contactRef = useRef<HTMLDivElement>(null);
   const homeRef = useRef<HTMLDivElement>(null);
-  const handleScroll = (ref: RefObject<HTMLDivElement>) => ref.current?.scrollIntoView({ behavior: 'smooth' }); 
+  const handleScroll = (ref: RefObject<HTMLDivElement>) => ref.current?.scrollIntoView({ behavior: 'smooth' });
   const defaultOnClickContato = () => handleScroll(contactRef)
   const defaultOnClickHome = () => handleScroll(homeRef)
   return (
@@ -45,14 +63,15 @@ export const Wireframe: FC<WireframeProps> = ({
       {/* header */}
       <div ref={homeRef}>
         <Header>
-          <NavItem to="/" onClick={onClickHome ?? defaultOnClickHome}>Home</NavItem>
-          { showQuemSomos   && <NavItem to="/#sobre-nos" onClick={onClickSomos}>Quem Somos</NavItem> }
-          { showOQueFazemos && <NavItem to="/services" onClick={onClickServicos}>Serviços</NavItem> }
-          { showUltimosAnos && <NavItem to="/#fizemos-recentemente" onClick={onClickUltimosAnos}>O que fizemos nos últimos anos</NavItem> }
-          <NavItem to="/#contato" onClick={onClickContato ?? defaultOnClickContato}>Contato</NavItem>
+          <NavItems
+            onClickHome={onClickHome ?? defaultOnClickHome}
+            onClickSomos={onClickSomos}
+            onClickServicos={onClickServicos}
+            onClickUltimosAnos={onClickUltimosAnos}
+            onClickContato={onClickContato ?? defaultOnClickContato}
+          />
         </Header>
       </div>
-
 
       {/* body of the page */}
       <Content>
@@ -62,11 +81,13 @@ export const Wireframe: FC<WireframeProps> = ({
       {/* footer */}
       <div ref={contactRef} id="contato">
         <Footer>
-          <NavItem to="/" onClick={onClickHome ?? defaultOnClickHome}>Home</NavItem>
-          { showQuemSomos   && <NavItem to="/#sobre-nos" onClick={onClickSomos}>Quem Somos</NavItem> }
-          { showOQueFazemos && <NavItem to="/services" onClick={onClickServicos}>Serviços</NavItem> }
-          { showUltimosAnos && <NavItem to="/#fizemos-recentemente" onClick={onClickUltimosAnos}>O que fizemos nos últimos anos</NavItem> }
-          <NavItem to="/#contato" onClick={onClickContato ?? defaultOnClickContato}>Contato</NavItem>
+          <NavItems
+            onClickHome={onClickHome ?? defaultOnClickHome}
+            onClickSomos={onClickSomos}
+            onClickServicos={onClickServicos}
+            onClickUltimosAnos={onClickUltimosAnos}
+            onClickContato={onClickContato ?? defaultOnClickContato}
+          />
         </Footer>
       </div>
     </Container>
